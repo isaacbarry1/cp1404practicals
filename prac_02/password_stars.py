80d53667fa39d0ce844dc8325aa8c5d2cf1772d2
isaacbarry1 <114851668+isaacbarry1@users.noreply.github.com>
2023-09-24 09:55:25 +1000

    Refactor password_stars.py

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,26 @@
+"""Password stars
+gets user to input password and confirm it meets the min lentgh
+than print the password in *
+"""
 
-password_length = 10
-password = input(f'Enter a password with a min length of {password_length} : ')
 
-while len(password) < password_length:
-    print("Password to short, Please enter a longer password")
+def get_password():
+    """Retrieves the password from the user and confirms it meets min length"""
+
+    password_length = 10
     password = input(f'Enter a password with a min length of {password_length} : ')
 
-print(len(password) * '*')
+    while len(password) < password_length:
+        print("Password to short, Please enter a longer password")
+        password = input(f'Enter a password with a min length of {password_length} : ')
 
+    return password
+
+
+def password_print(password):
+    """Prints the password in *"""
+    print(len(password) * '*')
+
+
+password = get_password()
+password_print(password)
