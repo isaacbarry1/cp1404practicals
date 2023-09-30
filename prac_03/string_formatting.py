496871f7a9340c13ffd7a89ec0cffcad0c17012f
isaacbarry1 <114851668+isaacbarry1@users.noreply.github.com>
2023-09-30 11:39:32 +1000

    Complete f printing and loop with left alignment.

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -32,12 +32,10 @@ numbers = [1, 19, 123, 456, -25]
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
-# TODO: Use f-string formatting to produce the output:
-# 1922 Gibson L-5 CES for about $16,035!
 
-# TODO: Using a for loop with the range function and string formatting,
-# produce the following right-aligned output (DO NOT use a list):
-#   0
-#  50
-# 100
-# 150
+# 1922 Gibson L-5 CES for about $16,035!
+print(f'{year} {name} for about ${cost}!')
+
+
+for i in range(0, 151, 50):
+    print(f'{i:3}')
