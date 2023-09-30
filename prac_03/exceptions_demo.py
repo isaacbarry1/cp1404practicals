0034ff57fdeca7542349a668308a6a882b71a524
isaacbarry1 <114851668+isaacbarry1@users.noreply.github.com>
2023-09-30 12:02:57 +1000

    Answer questions, and adjust code to catch when the denominator is 0.

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -1,14 +1,26 @@
 """
 CP1404/CP5632 - Practical
 Answer the following questions:
-1. When will a ValueError occur?
-2. When will a ZeroDivisionError occur?
+1. When will a ValueError occur? When a non int number is entered
+2. When will a ZeroDivisionError occur? When the denominator is 0
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
+
+
+def zero_check(denominator):
+    """Function to check if a number is 0"""
+    while denominator == 0:
+        print('Denominator cannot be Zero')
+        denominator = int(input("Enter the denominator: "))
+    return denominator
+
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+
+    denominator = zero_check(denominator)  # Function  to check if denominator is zero
+
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
@@ -16,4 +28,3 @@ except ValueError:
 except ZeroDivisionError:
     print("Cannot divide by zero!")
 print("Finished.")
-
