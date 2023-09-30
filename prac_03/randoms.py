bb45247bd9b2b5232711b07c5caeb237b4512faa
isaacbarry1 <114851668+isaacbarry1@users.noreply.github.com>
2023-09-30 11:46:50 +1000

    Answer questions for prac 3

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+"""
+What did you see in line 1?
+A random int between 5 and 20. With 20 being the largest
+
+What did you see in line 2?
+Prints a random int between 3 and 10 with a jump of 2
+The smallest was a 3 and the largest was a 9.
+A 4 could not be produced since it starts at 3 and jumps to every 2nd number until it reaches 10. 3, 5, 7, 9
+
+What did you see in line 3?
+It prints a random number between 2.5 and 5.5
+
+
+"""
+
+
