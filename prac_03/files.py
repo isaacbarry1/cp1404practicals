4ea1601887533c8fab3912e0ffb4882f0e72ea06
isaacbarry1 <114851668+isaacbarry1@users.noreply.github.com>
2023-09-30 12:15:18 +1000

    Open a file with the users name and read the file than print the name back to the user.

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,10 +2,12 @@
 CP1404/CP5632 - Practical
 """
 
+# Question 1
 name = input("Enter your name: ")
-
 with open(name + '.txt', 'w') as out_file:
     print(name, file=out_file)
 
-
-
+# Question 2
+with open(name + '.txt', 'r') as in_file:
+    name = in_file.read().strip()
+    print(f'Your name is {name}')
