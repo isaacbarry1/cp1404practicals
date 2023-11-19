26d73e17500e33ecbad145fb38e5fee98a99bc24
isaacbarry1 <114851668+isaacbarry1@users.noreply.github.com>
2023-11-19 12:18:49 +1000

    Test the taxi class

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -1,17 +1,19 @@
 """
 CP1404/CP5632 Practical
-Car class
+Taxi class, derived from Car
 """
+
 from prac_09.car import Car
 
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
+    price_per_km = 1.23
+
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -20,7 +22,9 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        # round fare result to nearest 10c
+        fare = round(self.price_per_km * self.current_fare_distance, 1)
+        return fare
 
     def start_fare(self):
         """Begin a new fare."""
